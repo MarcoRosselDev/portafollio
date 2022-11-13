9c5630e5f924d04b1309ed9b0c8ded41b89100c3
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-13 05:21:13 -0300

    structure pages and css 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Contact from "./pages/contact/Contact";
+
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
---
+++ src/components/Footer.jsx
@@ -0,0 +1,83 @@
+import { Link } from "react-router-dom";
+// import Logo from "../images/logo.png";
+// import { FaLinkedin } from "react-icons/fa";
+// import { ImFacebook } from "react-icons/im";
+// import { AiOutlineTwitter } from "react-icons/ai";
+// import { AiFillInstagram } from "react-icons/ai";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container footer__container">
+        <article>
+          <Link to="/" className="logo">
+            <img src="/" alt="Footer logo" />
+          </Link>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+            numquam sint rerum sequi nulla ipsam sapiente minima nostrum non.
+          </p>
+          <div className="footer__socials">
+            <a
+              href="http://linkedin.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {/* <FaLinkedin /> */}
+            </a>
+            <a
+              href="http://facebook.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {/* <ImFacebook /> */}
+            </a>
+            <a
+              href="http://twitter.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {/* <AiOutlineTwitter /> */}
+            </a>
+            <a
+              href="http://instagram.com/"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              {/* <AiFillInstagram /> */}
+            </a>
+          </div>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/about">About</Link>
+          <Link to="/plans">Plans</Link>
+          <Link to="/trainers">Trainers</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/about">About</Link>
+          <Link to="/plans">Plans</Link>
+          <Link to="/trainers">Trainers</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </article>
+        <article>
+          <h4>Permalinks</h4>
+          <Link to="/about">About</Link>
+          <Link to="/plans">Plans</Link>
+          <Link to="/trainers">Trainers</Link>
+          <Link to="/gallery">Gallery</Link>
+          <Link to="/contact">Contact</Link>
+        </article>
+      </div>
+      <div className="footer__copyright">
+        <small>2022 &copy; All Rights Reserved</small>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
