3e2d5f0eeb691f6df10cddac89539ab0ae70d820
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-16 16:34:41 -0300

    contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
 import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home";
 import Skills from "./components/skills/Skills";
@@ -13,6 +14,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Contact />
       </main>
     </>
   );
