c7318b83d02ae192b5d2b2f9f20cd9c078af719f
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-16 16:50:40 -0300

    contact page wsp msg and fb

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,8 +15,40 @@ const Contact = () => {
             <div className="contact__card">
               <i className="bx bx contact__card-icon"></i>
 
-              <h3 className="contact__card-title"></h3>
-              <span className="contact__card-data"></span>
+              <h3 className="contact__card-title">Email</h3>
+              <span className="contact__card-data">
+                andresmarcorossel@gmail.com
+              </span>
+
+              <a
+                href="mailto:andresmarcorossel@gmail.com.com"
+                className="contact__button"
+              >
+                Write me{" "}
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bx contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Wathsapp</h3>
+              <span className="contact__card-data">+569 51929740</span>
+
+              <a
+                href="https://api.whatsapp.com/send?phone=56951929740&text=Hello, more information!"
+                className="contact__button"
+              >
+                Write me{" "}
+                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+              </a>
+            </div>
+
+            <div className="contact__card">
+              <i className="bx bx contact__card-icon"></i>
+
+              <h3 className="contact__card-title">Messenger</h3>
+              <span className="contact__card-data">user.fb...</span>
 
               <a href="" className="contact__button">
                 Write me{" "}
@@ -25,6 +57,7 @@ const Contact = () => {
             </div>
           </div>
         </div>
+
         <div className="contact__content">
           <h3 className="contact__title">Write me your project</h3>
         </div>
