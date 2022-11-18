3f0ffe64aafc5a4aa81b6097e8b91fadd2cef856
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-17 22:01:49 -0300

    portfolio page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home";
+import Portfolio from "./components/portfolio/Portfolio";
 import ScrollUp from "./components/scrollup/ScrollUp";
 import Skills from "./components/skills/Skills";
 
@@ -16,6 +17,7 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Portfolio />
         <Contact />
       </main>
 
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./portfolio.css";
+
+const Portfolio = () => {
+  return (
+    <div id="portfolio" className="portfolio__container">
+      <h2 className="section__title">Portfolio</h2>
+      <span className="section__subtitle">Last Projects</span>
+    </div>
+  );
+};
+
+export default Portfolio;
