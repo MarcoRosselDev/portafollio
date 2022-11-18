c3a2e4837ea19725768335958f82a563a8d2c0f9
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-18 14:58:08 -0300

    dark mode 5

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./header.css";
+import useLocalStorage from "use-local-storage";
 
 const Header = () => {
   /*============= Change Background header ============= */
@@ -14,17 +15,28 @@ const Header = () => {
   const [activeNav, setActiveNav] = useState("#home");
 
   /*============= Dark mode ============= */
-  const [dark, setDark] = useState(false);
+  const defaultDark = window.matchMedia(
+    "(prefers-color-scheme: header)"
+  ).matches;
+  const [theme, setTheme] = useLocalStorage(
+    "theme",
+    defaultDark ? "header" : "header-dark"
+  );
+
+  const switchTheme = () => {
+    const newTheme = theme === "header" ? "header" : "header-dark";
+    setTheme(newTheme);
+  };
 
   return (
     // <header className="header">
-    <header className={!setDark ? "header" : "header-dark"}>
+    <header className="header" data-theme={theme}>
       <nav className="nav container">
         <div className="left-side">
           <a href="index.html" className="nav__logo">
             Marco
           </a>
-          <div className="dark-button" onClick={() => setDark(true)}>
+          <div className="dark-button" onClick={switchTheme}>
             <div className="button-bw"></div>
           </div>
         </div>
