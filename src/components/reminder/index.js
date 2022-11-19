32567160902c439e89e8c80cc7a522aff53778da
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-19 19:26:50 -0300

    reminder localStorage

---
+++ src/components/reminder/index.js
@@ -0,0 +1,9 @@
+/* 
+    <input type="text" placeholder="tipe something" />
+    <button class="toDoButton">send</button>
+    <button class="cleanButton">clean</button>
+    <button class="cleanAllButton">clean all</button>
+
+    <ul class="ulPut"></ul>
+
+*/
