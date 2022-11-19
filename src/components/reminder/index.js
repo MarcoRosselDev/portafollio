0e8351644331af33eaf73bb873dc9afdb7376eac
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-19 19:36:27 -0300

    reminder localStorage 2

--- src/components/reminder/index.js
+++ src/components/reminder/index.js
@@ -7,3 +7,17 @@
     <ul class="ulPut"></ul>
 
 */
+
+const inputText = document.querySelector(".inputText");
+const toDoButton = document.querySelector(".toDoButton");
+const cleanButton = document.querySelector(".cleanButton");
+const cleanAllButton = document.querySelector(".cleanAllButton");
+const ulPut = document.querySelector(".ulPut");
+
+let inputTextValue = [];
+
+toDoButton.addEventListener("click", function () {
+  inputTextValue = inputText.value;
+  ulPut.innerHTML += `<li>${inputTextValue}</li>`;
+  inputText.value = "";
+});
