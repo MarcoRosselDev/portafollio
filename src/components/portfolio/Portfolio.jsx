c1b5ed4adc413d3937226a8c94663c556a3cdf24
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-21 20:58:14 -0300

    portfolio projects button center fixed

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,17 +5,21 @@ import ProjectTwo from "./ProjectTwo";
 
 const Portfolio = () => {
   return (
-    <div id="portfolio" className="portfolio__container section container">
-      <h2 className="section__title">Portfolio</h2>
-      <span className="section__subtitle">Last Projects</span>
-      <div className="portfolio__container container grid">
-        <ProjectOne />
-        <ProjectTwo />
+    <>
+      <div id="portfolio" className="portfolio__container section container">
+        <h2 className="section__title">Portfolio</h2>
+        <span className="section__subtitle">Last Projects</span>
+        <div className="portfolio__container  grid">
+          <ProjectOne />
+          <ProjectTwo />
+        </div>
       </div>
-      <button className="button button--flex button-projects">
-        See All Projects
-      </button>
-    </div>
+      <div className="qwe">
+        <button className="button button--flex button-projects">
+          See All Projects
+        </button>
+      </div>
+    </>
   );
 };
 
