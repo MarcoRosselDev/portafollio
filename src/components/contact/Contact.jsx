3cb1bf5871c6e6c30702c44e7a092e24e6f1b249
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-25 04:59:24 -0300

    footer boxicon successful

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -87,7 +87,6 @@ const Contact = (dark) => {
             </div>
 
             <div className="contact__card">
-              {/* <i className="bx bxl-messenger contact__card-icon"></i> */}
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="24"
