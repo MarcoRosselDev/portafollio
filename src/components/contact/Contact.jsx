63b222984c9b6bf799e8ec36cd74eaa723bb57bb
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-11-25 04:47:16 -0300

    contact boxicon successful

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
 import "./contact.css";
+import ArrowLeft from "./ArrowLeft";
 
 const Contact = (dark) => {
   const form = useRef();
@@ -38,7 +39,7 @@ const Contact = (dark) => {
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
-                className="boxicon-mail "
+                className="boxicon-mail contact__card-icon"
               >
                 <path d="M20 4H6c-1.103 0-2 .897-2 2v5h2V8l6.4 4.8a1.001 1.001 0 0 0 1.2 0L20 8v9h-8v2h8c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm-7 6.75L6.666 6h12.668L13 10.75z" />
                 <path d="M2 12h7v2H2zm2 3h6v2H4zm3 3h4v2H7z" />
@@ -53,13 +54,25 @@ const Contact = (dark) => {
                 href="mailto:andresmarcorossel@gmail.com.com"
                 className="contact__button"
               >
-                Write me{" "}
-                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                Write me
+                <ArrowLeft />
               </a>
             </div>
 
             <div className="contact__card">
-              <i className="bx bxl-whatsapp contact__card-icon"></i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="boxicon-mail contact__card-icon"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M18.403 5.633A8.919 8.919 0 0 0 12.053 3c-4.948 0-8.976 4.027-8.978 8.977 0 1.582.413 3.126 1.198 4.488L3 21.116l4.759-1.249a8.981 8.981 0 0 0 4.29 1.093h.004c4.947 0 8.975-4.027 8.977-8.977a8.926 8.926 0 0 0-2.627-6.35m-6.35 13.812h-.003a7.446 7.446 0 0 1-3.798-1.041l-.272-.162-2.824.741.753-2.753-.177-.282a7.448 7.448 0 0 1-1.141-3.971c.002-4.114 3.349-7.461 7.465-7.461a7.413 7.413 0 0 1 5.275 2.188 7.42 7.42 0 0 1 2.183 5.279c-.002 4.114-3.349 7.462-7.461 7.462m4.093-5.589c-.225-.113-1.327-.655-1.533-.73-.205-.075-.354-.112-.504.112s-.58.729-.711.879-.262.168-.486.056-.947-.349-1.804-1.113c-.667-.595-1.117-1.329-1.248-1.554s-.014-.346.099-.458c.101-.1.224-.262.336-.393.112-.131.149-.224.224-.374s.038-.281-.019-.393c-.056-.113-.505-1.217-.692-1.666-.181-.435-.366-.377-.504-.383a9.65 9.65 0 0 0-.429-.008.826.826 0 0 0-.599.28c-.206.225-.785.767-.785 1.871s.804 2.171.916 2.321c.112.15 1.582 2.415 3.832 3.387.536.231.954.369 1.279.473.537.171 1.026.146 1.413.089.431-.064 1.327-.542 1.514-1.066.187-.524.187-.973.131-1.067-.056-.094-.207-.151-.43-.263"
+                />
+              </svg>
 
               <h3 className="contact__card-title">Wathsapp</h3>
               <span className="contact__card-data">+569 51929740</span>
@@ -68,20 +81,29 @@ const Contact = (dark) => {
                 href="https://api.whatsapp.com/send?phone=56951929740&text=Hello, more information!"
                 className="contact__button"
               >
-                Write me{" "}
-                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                Write me
+                <ArrowLeft />
               </a>
             </div>
 
             <div className="contact__card">
-              <i className="bx bxl-messenger contact__card-icon"></i>
+              {/* <i className="bx bxl-messenger contact__card-icon"></i> */}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="boxicon-mail contact__card-icon"
+              >
+                <path d="M12 3c-4.92 0-8.91 3.729-8.91 8.332 0 2.616 1.291 4.952 3.311 6.479V21l3.041-1.687c.811.228 1.668.35 2.559.35 4.92 0 8.91-3.73 8.91-8.331C20.91 6.729 16.92 3 12 3zm.938 11.172-2.305-2.394-4.438 2.454 4.865-5.163 2.305 2.395 4.439-2.455-4.866 5.163z" />
+              </svg>
 
               <h3 className="contact__card-title">Messenger</h3>
               <span className="contact__card-data">user.fb...</span>
 
               <a href="https://m.me/crypticalcoder" className="contact__button">
-                Write me{" "}
-                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
+                Write me
+                <ArrowLeft />
               </a>
             </div>
           </div>
